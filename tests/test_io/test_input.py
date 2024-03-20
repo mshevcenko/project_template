684e9fa3be41f20f8f3863c1ccd47bc305cf6ac2
Mykhailo Shevchenko <muhailo11111@gmail.com>
2024-03-20 20:32:19 +0200

    Add tests for read_csv_from_file_with_pandas

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -16,3 +16,20 @@ class ReadTextFromFileTest(unittest.TestCase):
     def test_read_from_non_existent_file(self):
         with self.assertRaises(FileNotFoundError):
             read_text_from_file("../../test_data/non_existent.csv")
+
+
+class ReadCsvFromFileWithPandasTest(unittest.TestCase):
+
+    def test_read_from_file(self):
+        data = read_csv_from_file_with_pandas("../../test_data/test.csv")
+        dict_from_file = data.to_dict()
+        self.assertEqual({'Name': {0: 'January', 1: 'February', 2: 'March'},
+                          'Abbreviation': {0: 'Jan.', 1: 'Feb.', 2: 'Mar.'}}, dict_from_file)
+
+    def test_read_from_empty_file(self):
+        with self.assertRaises(pd.errors.EmptyDataError):
+            read_csv_from_file_with_pandas("../../test_data/empty.csv")
+
+    def test_read_from_non_existent_file(self):
+        with self.assertRaises(FileNotFoundError):
+            read_csv_from_file_with_pandas("../../test_data/non_existent.csv")
