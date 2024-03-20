cd4ff71121bb0b88732c838f17ba13d0a48b40be
Mykhailo Shevchenko <muhailo11111@gmail.com>
2024-03-20 13:52:22 +0200

    Created empty functions to read and write

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def read_text_from_terminal(placeholder="Write text:"):
+    pass
+
+
+def read_text_from_file(filepath):
+    pass
+
+
+def read_csv_from_file_with_pandas(filepath):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def write_text_to_terminal(text):
+    pass
+
+
+def write_text_to_file(text, filepath):
+    pass
+
+
+def write_csv_to_file_with_pandas(df, filepath):
+    pass
